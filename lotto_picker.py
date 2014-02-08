e959e2238abe52578d748e049f51e35835abaf2c
madflytom <pi@raspberrypi>
2014-02-08 11:25:43 -0500

    first commit

---
+++ lotto_picker.py
@@ -0,0 +1,9 @@
+#3_1_dice
+import random
+print("PowerBall Numbers: ")
+for x in range(1,6):
+    random_number = random.randint(1,30)
+    print(random_number)
+print("PowerBall: ")
+random_powerball = random.randint(1,16)
+print(random_powerball)
